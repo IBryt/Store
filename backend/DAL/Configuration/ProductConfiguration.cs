a51dd79bfd41d4b2130cb7da3bf117b92ee1f170
ihorbryt@gmail.com <36583283+IBryt@users.noreply.github.com>
2023-12-20 14:36:23 +0200

    add fluent api configuration

---
+++ backend/DAL/Configuration/ProductConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using IgorBryt.Store.DAL.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IgorBryt.Store.DAL.Configuration;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder.Property(p => p.ProductCategoryId)
+            .IsRequired();
+
+        builder.Property(p => p.ProductName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(p => p.Description)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(p => p.ImageUrl)
+           .HasMaxLength(50);
+
+        builder.Property(p => p.Price)
+            .IsRequired();
+    }
+}
